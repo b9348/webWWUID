de312fdd240476da614500c324692a9a1da3b536
b9348 <boris9348@qq.com>
2025-02-05 08:56:45 +0800

    v3

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
-import { Button, Form, Input } from 'antd'
+import { useState, useEffect } from 'react'
+import { Button, Form, Input, InputNumber } from 'antd'
+const { TextArea } = Input;
 import styles from './index.module.less'
 
 function Login() {
@@ -8,6 +9,13 @@ function Login() {
   const [towerData, setTowerData] = useState('')
   const [towerRender, setTowerRender] = useState(false)
 
+  const [charJson, setCharJson] = useState({})
+
+  useEffect(() => {
+    fetch('https://api.hakush.in/ww/data/character.json')
+      .then(response => response.json())
+      .then(data => setCharJson(data))
+  }, [])
 
   const onFinish = () => {
     console.log('saveDataToBrowser');
@@ -21,7 +29,7 @@ function Login() {
   }
 
   const fetchTest = async () => {
-    const url = 'https://api.kurobbs.com/aki/roleBox/akiBox/towerIndex'
+    const url = 'https://api.kurobbs.com/aki/roleBox/akiBox/towerDataDetail'
     const headers = {
       "Content-Type": "application/x-www-form-urlencoded",
       "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0",
@@ -61,9 +69,10 @@ function Login() {
       const rsp = await response.json()
 
       if (rsp.code === 200) {
-        setTowerData(JSON.parse(rsp.data))
+        const res = JSON.parse(rsp.data)
+        console.log(res.difficultyList[3]);
+        setTowerData(res)
         setTowerRender(true)
-        console.log(JSON.parse(rsp.data));
       } else {
         alert(rsp.msg)
         console.error('api error:', JSON.stringify(rsp))
@@ -73,14 +82,13 @@ function Login() {
     }
   }
 
-  const tower = towerData.difficultyList && towerData.difficultyList[0]
+  const tower = towerData.difficultyList && towerData.difficultyList[3]
   return (
     <div className={styles.wrapper}>
-
       <Form
         name="basic"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
+        // labelCol={{ span: 2 }}
+        // wrapperCol={{ span: 16 }}
         initialValues={{ remember: true }}
         onFinish={onFinish}
         onFinishFailed={onFinishFailed}
@@ -89,51 +97,81 @@ function Login() {
         <Form.Item
           label="UID"
           name="uid"
+          type="number"
           rules={[{ required: true, message: 'Please input your UID!' }]}
         >
-          <Input value={uid} defaultValue={localStorage.uid ? localStorage.uid : uid} onChange={(e) => setUid(e.target.value)} />
+          <InputNumber style={{ width: '100%' }} size={'large'} value={uid} defaultValue={localStorage.uid ? localStorage.uid : uid} onChange={(e) => setUid(e.target.value)} />
         </Form.Item>
-
         <Form.Item
           label="Token"
           name="token"
           rules={[{ required: true, message: 'Please input your token!' }]}
         >
-          <Input value={token} defaultValue={localStorage.token ? localStorage.token : token} onChange={(e) => setToken(e.target.value)} />
+          <TextArea rows={6} value={token} defaultValue={localStorage.token ? localStorage.token : token} onChange={(e) => setToken(e.target.value)} />
         </Form.Item>
-
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Form.Item >
           <Button type="primary" htmlType="submit">
             保存
           </Button>
-          <p>保存后token会保存在浏览器本地，token过期之前不用再填</p>
         </Form.Item>
       </Form>
 
+      <p>保存后token会保存在浏览器本地，token过期之前不用再填</p>
+      {/* 深境区tower     深境之塔area */}
       <button onClick={fetchTest}>查询</button>
-      {towerRender && <div className={styles.tower}>
+      <div className={styles.BGI} style={{
+        backgroundImage: `url(https://cloudflare-imgbed-4n1.pages.dev/file/bg4.jpg)`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}>
 
-        <div className={styles.towerTitle}>{tower.difficultyName}</div>
-        {tower.towerAreaList.map(area => {
-          return <div key={area.areaId} className={styles.areaWrapper}>
-            <div className={styles.areaTitle}>{area.areaName} 第{area.floorList[0].floor}层</div>
-            <div className={styles.areaContent}>
-              <div className={styles.star}>
-                {area.floorList[0].star} ★
-              </div>
-              <div className={styles.char}>
-                {area.floorList[0].roleList.map((role, idx) => {
-                  return <div key={idx} className={styles.roleBox}>
-                    {/* <div className={styles.roleName}>{role.roleName}</div> */}
-                    <div className={styles.roleImg}><img src={role.iconUrl} /></div>
-                  </div>
-                })}
+        {towerRender && <div className={styles.tower} >
+          {tower.towerAreaList.map(area => {
+            return <div key={area.areaId} className={styles.areaWrapper}>
+              <div className={styles.areaInfo}>
+                <div className={styles.areaName}>{tower.difficultyName}-{area.areaName}{area.star}/{area.maxStar}</div>
+                <div className={styles.areaFloors}>
+                  {area.floorList.map(floor => {
+                    return <div key={floor.floor} className={styles.floorBox}
+                      style={{
+                        backgroundImage: `url(${floor.picUrl})`,
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                      }}>
+                      <div className={styles.floorBoxLeft}>
+                        <div className={styles.floorName}>第 {floor.floor} 层</div>
+                        <div className={styles.floorStar}>
+                          {Array.from({ length: floor.star }).map((_, i) => <img key={i} className={styles.floorStarFull} src="https://cloudflare-imgbed-4n1.pages.dev/file/star_full.png" />)}
+                          {Array.from({ length: floor.maxStar - floor.star }).map((_, i) => <img key={i} className={styles.floorStarEmpty} src="https://cloudflare-imgbed-4n1.pages.dev/file/star_empty.png" />)}
+
+                        </div>
+                      </div>
+                      <div className={styles.floorBoxRight}>
+                        {floor.roleList.map(role => {
+                          return <div key={role.roleId} className={styles.roleBox}>
+                            <img className={styles.roleRankBg} src={charJson[role.roleId].rank === 5 ? "https://cloudflare-imgbed-4n1.pages.dev/file/char_bg5.png" : "https://cloudflare-imgbed-4n1.pages.dev/file/char_bg4.png"} />
+                            <div className={styles.circleMask}>
+                              <div className={styles.maskShow}>
+                                <div className={styles.maskCut}>
+                                  <img className={styles.roleIconUrl} src={role.iconUrl} />
+                                </div>
+                              </div>
+                            </div>
+                            <div className={styles.roleName}>
+                              <span>{charJson[role.roleId]?.['zh-Hans'] || ''} </span>
+                            </div>
+                          </div>
+                        })}
+                      </div>
+                    </div>
+                  })}
+                </div>
               </div>
             </div>
-          </div>
-        })}
+          })}
 
-      </div>}
+        </div>}
+      </div>
     </div>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import postcssPxtorem from 'postcss-pxtorem'
+import autoprefixer from 'autoprefixer'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        postcssPxtorem({
+          rootValue: 16,
+          unitPrecision: 5,
+          propList: ['*'],
+          selectorBlackList: [],
+          replace: true,
+          mediaQuery: false,
+          minPixelValue: 0,
+        }),
+        autoprefixer({
+          overrideBrowserslist: ['> 1%', 'last 2 versions', 'not dead', 'iOS >= 9'],
+        }),
+      ],
+    },
+  },
 })
