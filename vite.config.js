4308651f22084b65332c40f0e1ed1a184fb8ac61
b9348 <boris9348@qq.com>
2025-02-06 23:26:27 +0800

    3

--- vite.config.js
+++ vite.config.js
@@ -3,30 +3,32 @@ import react from '@vitejs/plugin-react'
 import postcssPxtorem from 'postcss-pxtorem'
 import autoprefixer from 'autoprefixer'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],  
-  // eslint-disable-next-line no-undef
-  base: process.env.VITE_BASE_PATH || '/',
-  build: {
-    outDir: 'dist',
-  },
-  css: {
-    postcss: {
-      plugins: [
-        postcssPxtorem({
-          rootValue: 16,
-          unitPrecision: 5,
-          propList: ['*'],
-          selectorBlackList: [],
-          replace: true,
-          mediaQuery: false,
-          minPixelValue: 0,
-        }),
-        autoprefixer({
-          overrideBrowserslist: ['> 1%', 'last 2 versions', 'not dead', 'iOS >= 9'],
-        }),
-      ],
+export default defineConfig((command) => {
+  const base = command === 'build:gh' ? '/webWWUID/' : '/';
+
+  return {
+    plugins: [react()],
+    base,
+    build: {
+      outDir: 'dist'
     },
-  },
+    css: {
+      postcss: {
+        plugins: [
+          postcssPxtorem({
+            rootValue: 16,
+            unitPrecision: 5,
+            propList: ['*'],
+            selectorBlackList: [],
+            replace: true,
+            mediaQuery: false,
+            minPixelValue: 0,
+          }),
+          autoprefixer({
+            overrideBrowserslist: ['> 1%', 'last 2 versions', 'not dead', 'iOS >= 9'],
+          }),
+        ],
+      },
+    },
+  }
 })
